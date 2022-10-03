8919edb128fa2b0a92eb0273fb926a2a6080b403
conormcmichael <114887784+conormcmichael@users.noreply.github.com>
2022-10-02 20:44:14 -0400

    Update ContentView.swift

--- learning-github/ContentView.swift
+++ learning-github/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, town!")
+            Text("What's up, town!?")
         }
         .padding()
     }
