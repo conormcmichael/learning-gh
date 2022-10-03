f2a8146936d7ef71236a951e67c8f9786967d51f
Conor McMichael <conor.jack.mcmichael@gmail.com>
2022-10-02 20:26:38 -0400

    Initial Commit

---
+++ learning-github/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  learning-github
+//
+//  Created by Conor McMichael on 10/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learning-github/learning_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_githubApp.swift
+//  learning-github
+//
+//  Created by Conor McMichael on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct learning_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
